fadc5d5ddaccfa02628fbc6e350908e9792e1cd0
femarko <fenstermark7@gmail.com>
2026-08-15 02:54:57 +0200

    refactor(db): migrate ORM models to SQLAlchemy 2.0 typing

--- src/payments/infrastructure/db/orm_models.py
+++ src/payments/infrastructure/db/orm_models.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import (
     DeclarativeBase,
     relationship,
+    Mapped,
+    mapped_column,
 )
 from sqlalchemy import (
     Column,
@@ -11,8 +13,20 @@ from sqlalchemy import (
     Boolean,
 )
 from sqlalchemy.dialects.postgresql import UUID as sqlalch_uuid
-from typing import TypeVar
+from typing import (
+    TypeVar,
+    Optional
+)
+from decimal import Decimal
+from uuid import UUID
+from datetime import datetime
+
 from payments.domain.value_objects import MoneyConfig
+from payments.domain.enums import (
+    PaymentStatus,
+    PaymentType,
+    Currency,
+)
 
 
 class SQLAlchBaseModel(DeclarativeBase): ...
@@ -23,41 +37,41 @@ T_SQLAlchBaseModel = TypeVar("T_SQLAlchBaseModel", bound=type[SQLAlchBaseModel])
 
 class SQLAlchPaymentModel(SQLAlchBaseModel):
     __tablename__ = "payments"
-    id = Column(sqlalch_uuid(as_uuid=True), primary_key=True, index=True)
-    type = Column(String, index=True, nullable=False)
-    status = Column(String, index=True, nullable=False)
-    amount = Column(
+    id: Mapped[UUID] = mapped_column(sqlalch_uuid(as_uuid=True), primary_key=True, index=True)
+    type: Mapped[PaymentType] = mapped_column(String, index=True, nullable=False)
+    status: Mapped[PaymentStatus] = mapped_column(String, index=True, nullable=False)
+    amount: Mapped[Decimal] = mapped_column(
         Numeric(MoneyConfig.PRECISION, MoneyConfig.SCALE, asdecimal=True),
         nullable=False
     )
-    currency = Column(String, index=True, nullable=False)
-    order_id = Column(
+    currency: Mapped[Currency] = mapped_column(String, index=True, nullable=False)
+    order_id: Mapped[UUID] = mapped_column(
         sqlalch_uuid(as_uuid=True),
         ForeignKey("orders.id", ondelete="CASCADE"),
         nullable=False,
         index=True
     )
-    created_at = Column(DateTime(timezone=True), nullable=False)
-    updated_at = Column(DateTime(timezone=True))
-    external_id = Column(String, index=True, nullable=True, unique=True)
-    is_accepted = Column(Boolean, index=True, nullable=False, default=False)
-    is_refunded = Column(Boolean, index=True, nullable=False, default=False)
-    accepted_at = Column(DateTime(timezone=True))
-    refunded_at = Column(DateTime(timezone=True))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    updated_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
+    external_id: Mapped[Optional[str]] = mapped_column(String, index=True, nullable=True, unique=True)
+    is_accepted: Mapped[bool] = mapped_column(Boolean, index=True, nullable=False, default=False)
+    is_refunded: Mapped[bool] = mapped_column(Boolean, index=True, nullable=False, default=False)
+    accepted_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
+    refunded_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
     order = relationship("SQLAlchOrderModel", back_populates="payments")
 
 
 class SQLAlchOrderModel(SQLAlchBaseModel):
     __tablename__ = "orders"
-    id = Column(sqlalch_uuid(as_uuid=True), primary_key=True, index=True)
-    total_amount = Column(
+    id: Mapped[UUID] = mapped_column(sqlalch_uuid(as_uuid=True), primary_key=True, index=True)
+    total_amount: Mapped[Decimal] = mapped_column(
         Numeric(MoneyConfig.PRECISION, MoneyConfig.SCALE, asdecimal=True),
         nullable=False
     )
-    currency = Column(String, index=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), nullable=False)
-    updated_at = Column(DateTime(timezone=True))
-    paid_amount = Column(
+    currency: Mapped[Currency] = mapped_column(String, index=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    updated_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
+    paid_amount: Mapped[Decimal] = mapped_column(
         Numeric(MoneyConfig.PRECISION, MoneyConfig.SCALE, asdecimal=True),
         nullable=False
     )
